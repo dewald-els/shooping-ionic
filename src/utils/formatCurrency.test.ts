b9033d10d5be9a87e9cfb9fe2f6b1bed8656152e
Dewald Els <dewaldifels@gmail.com>
2023-09-07 18:16:31 +0200

    feat: Testing init

--- src/utils/formatCurrency.test.ts
+++ src/utils/formatCurrency.test.ts
@@ -2,18 +2,14 @@ import formatCurrency from "./formatCurrency";
 
 describe("formatCurrency", () => {
   it("formats positive currency correctly", () => {
-    const result = formatCurrency(1234);
-    const expected = "R 1 234,00";
-    console.log(result.trim(), expected);
-
-    expect(result).toBe(expected);
+    expect(formatCurrency(1234.56)).toBe("R1,234.56");
   });
 
   it("formats negative currency correctly", () => {
-    expect(formatCurrency(-1234.56)).toBe("-R 1 234,56");
+    expect(formatCurrency(-1234.56)).toBe("-R1,234.56");
   });
 
   it("formats zero currency correctly", () => {
-    expect(formatCurrency(0)).toBe("R 0,00");
+    expect(formatCurrency(0)).toBe("R0.00");
   });
 });
